d0c7033eb6bc14819560ba49065b7c61785f6de7
YangKelvin <k31093109@gmail.com>
2018-12-12 14:04:38 +0800

    add init user

--- TTTS/user.py
+++ TTTS/user.py
@@ -21,6 +21,34 @@ def login_required(view):
 # blueprint name='user'
 bp = Blueprint('user', __name__, url_prefix='/user')
 
+# test
+# register
+@bp.route('/init', methods=('GET', 'POST'))
+def init():
+    db = get_db()
+    db.execute(
+        'INSERT INTO ACCOUNT (Account, Password, PermissionID, UserName, IdentificationNumber, Gender, CellphoneNumber, Email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        ('105590045', generate_password_hash('123'), '1', 'kelvin', 'N123456789', 'M', '0975107900', 't105590045@ntut.org.tw')
+    )
+    db.execute(
+        'INSERT INTO ACCOUNT (Account, Password, PermissionID, UserName, IdentificationNumber, Gender, CellphoneNumber, Email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        ('105590023', generate_password_hash('234'), '2', 'james', 'N123098765', 'M', '0978700387', 't105590023@ntut.org.tw')
+    )
+    db.execute(
+        'INSERT INTO ACCOUNT (Account, Password, PermissionID, UserName, IdentificationNumber, Gender, CellphoneNumber, Email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        ('105590021', generate_password_hash('345'), '2', 'kelvin', 'N123098764', 'M', '0912345678', 't105590021@ntut.org.tw')
+    )
+    db.execute(
+        'INSERT INTO ACCOUNT (Account, Password, PermissionID, UserName, IdentificationNumber, Gender, CellphoneNumber, Email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        ('105590014', generate_password_hash('456'), '3', 'kelvin', 'N123098765', 'M', '0987654321', 't105590014@ntut.org.tw')
+    )
+    db.execute(
+        'INSERT INTO ACCOUNT (Account, Password, PermissionID, UserName, IdentificationNumber, Gender, CellphoneNumber, Email) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        ('105590000', generate_password_hash('567'), '3', 'kelvin', 'N000000000', 'M', '0900000000', 't105590000@ntut.org.tw')
+    )
+    db.commit()
+    return redirect(url_for('user.login'))
+
 # register
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
