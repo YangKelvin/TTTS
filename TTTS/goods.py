6e7d55f2727cb0ca997f13f73f7ba0b60ac182cb
YangKelvin <k31093109@gmail.com>
2018-12-17 13:13:04 +0800

    add goods

--- TTTS/goods.py
+++ TTTS/goods.py
@@ -26,4 +26,46 @@ def init():
         ('好好喝烏龍茶', '烏龍茶', '100', '5', '好喝的綠茶喔', 'goodGreenTea.png', '1')
     )
     db.commit()
-    return redirect(url_for('user.login'))
+    return redirect(url_for('goods.addNewGoods'))
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT GoodsName, GoodsType, Price, StockQuantity, Introduction, ImageName, CountryOfOrigin'
+        ' FROM GOODS'
+        ' ORDER BY GoodsID DESC'
+    ).fetchall()
+    return render_template('goods/index.html', posts=posts)
+
+
+@bp.route('/addNewGoods', methods=('GET', 'POST'))
+# @login_required
+def addNewGoods():
+    if request.method == 'POST':
+        goodsName = request.form['goodsName']
+        goodsType = request.form['goodsType']
+        price = request.form['price']
+        stockQuantity = request.form['stockQuantity']
+        introduction = request.form['introduction']
+        imageName = request.form['imageName']
+        countryOfOrigin = request.form['countryOfOrigin']
+
+        error = None
+
+        if not goodsName:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO GOODS (GoodsName, GoodsType, Price, StockQuantity, Introduction, ImageName, CountryOfOrigin)'
+                ' VALUES (?, ?, ?, ?, ?, ?, ?)',
+                (goodsName, goodsType, price, stockQuantity, introduction, imageName, countryOfOrigin)
+            )
+            db.commit()
+            return redirect(url_for('goods.addNewGoods'))
+
+    return render_template('goods/addNewGoods.html')
