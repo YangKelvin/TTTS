5b058d83c6083e197def2170fdef2ccf37285901
Jing-Xun-Lin <t105590021@ntut.org.tw>
2018-12-19 19:15:44 +0800

    Create search.py
    新增搜尋關鍵字功能

---
+++ TTTS/search.py
@@ -0,0 +1,22 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for, session
+)
+from TTTS.db import get_db
+
+bp = Blueprint('search', __name__, url_prefix='search')
+
+@bp.route('/', methods=('GET', 'POST'))
+def search():
+    if request.method == 'POST':
+        db = get_db
+        name = request.form['searchName']
+
+        goods = db.execute(
+            'SELECT * FROM GOODS AS G'
+            ' WHERE G.GoodsName = ?',
+            ('%' + name + '%',)
+        ).fetchall()
+        # 要改
+        return render_template('search/search.html', post=goods)
+    # 要改
+    return render_template('search/search.html')
