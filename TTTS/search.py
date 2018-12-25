f5bffd69841094b3e820defebb47f23ca0deacfb
Jing-Xun-Lin <t105590021@ntut.org.tw>
2018-12-25 18:28:20 +0800

    Update search.py
    修改search功能

--- TTTS/search.py
+++ TTTS/search.py
@@ -5,17 +5,14 @@ from TTTS.db import get_db
 
 bp = Blueprint('search', __name__, url_prefix='/search')
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/', methods=('POST',))
 def search():
-    if request.method == 'POST':
-        db = get_db()
-        name = request.form['searchName']
-        goods = db.execute(
-            'SELECT * FROM GOODS'
-            ' WHERE GoodsName LIKE ?',
-            ('%' + name + '%',)
-        ).fetchall()
-        # 要改
-        return render_template('search/searchResult.html', posts=goods)
-    # 要改
-    return render_template('search/search.html')
+    db = get_db()
+    name = request.form['searchName']
+    goods = db.execute(
+        'SELECT * FROM GOODS'
+        ' WHERE GoodsName LIKE ?',
+        ('%' + name + '%',)
+    ).fetchall()
+
+    return render_template('search/searchResult.html', posts=goods)
