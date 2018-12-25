8f3c4a65be2c371ce897968b9aa7d7e659192859
Jing-Xun-Lin <t105590021@ntut.org.tw>
2018-12-25 19:10:24 +0800

    Update __init__.py
    修改初始頁面

--- TTTS/__init__.py
+++ TTTS/__init__.py
@@ -23,10 +23,10 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+    
+    @app.route('/TTTS')
+    def TTTS():
+        return render_template('TTTS.html')
 
     from . import db
     db.init_app(app)
@@ -42,7 +42,7 @@ def create_app(test_config=None):
 
     from . import goods
     app.register_blueprint(goods.bp)
-    app.add_url_rule('/', endpoint='index', view_func=index)
+    app.add_url_rule('/', endpoint='TTTS', view_func=TTTS)
 
     @app.route('/initDatabase')
     def initDataBase():
